0f0ca5c931c23fb0760b16ca4fb680e634623fc6
terrep263 <94027341+terrep263@users.noreply.github.com>
2025-08-20 13:57:47 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  envPrefix: ['VITE_', 'REACT_APP_']
+  plugins: [react({
+    jsxRuntime: 'automatic'
+  })],
+  envPrefix: ['VITE_', 'REACT_APP_'],
+  esbuild: {
+    jsx: 'automatic',
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment'
+  }
 })
