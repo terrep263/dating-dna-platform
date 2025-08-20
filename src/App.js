aff4883ee0915c50ffd4f2fdec163544714e9b47
terrep263 <94027341+terrep263@users.noreply.github.com>
2025-08-20 17:33:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import { motion, AnimatePresence } from 'framer-motion';
 import { initializeAffiliateTracking } from './utils/affiliateTracking';
+
+// Import your components
 import HomePage from './components/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HowItWorks from './components/HowItWorks';
+import DNATypes from './components/DNATypes';
+import Premium from './components/Premium';
+import PremiumSingles from './components/PremiumSingles';
+import PremiumCouples from './components/PremiumCouples';
+import SinglesAssessmentApp from './components/assessment/SinglesAssessmentApp';
+import CouplesAssessmentApp from './components/assessment/CouplesAssessmentApp';
+import Results from './components/Results';
+import FreeAssessment from './components/FreeAssessment';
 
 const AppContainer = styled.div`
   min-height: 100vh;
@@ -36,6 +47,15 @@ function App() {
         >
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/how-it-works" element={<HowItWorks />} />
+            <Route path="/dna-types" element={<DNATypes />} />
+            <Route path="/premium" element={<Premium />} />
+            <Route path="/premium-singles" element={<PremiumSingles />} />
+            <Route path="/premium-couples" element={<PremiumCouples />} />
+            <Route path="/assessment" element={<SinglesAssessmentApp />} />
+            <Route path="/assessment/couples" element={<CouplesAssessmentApp />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/free-assessment" element={<FreeAssessment />} />
           </Routes>
         </MainContent>
       </AnimatePresence>
