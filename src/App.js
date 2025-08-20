fb461797c3520beb4d840bd1556b29d2f0a7a0a8
terrep263 <94027341+terrep263@users.noreply.github.com>
2025-08-20 15:01:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import { motion, AnimatePresence } from 'framer-motion';
 import { initializeAffiliateTracking } from './utils/affiliateTracking';
-
-// Import your actual components
 import HomePage from './components/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import SinglesAssessmentApp from './components/assessment/SinglesAssessmentApp';
-import CouplesAssessmentApp from './components/assessment/CouplesAssessmentApp';
-import Results from './components/Results';
-import HowItWorks from './components/HowItWorks';
-import DNATypes from './components/DNATypes';
-import Premium from './components/Premium';
-import PremiumSingles from './components/PremiumSingles';
-import PremiumCouples from './components/PremiumCouples';
 
 const AppContainer = styled.div`
   min-height: 100vh;
@@ -45,4 +35,13 @@ function App() {
           transition={{ duration: 0.3 }}
         >
           <Routes>
-            <Route path="/" elem
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </MainContent>
+      </AnimatePresence>
+      <Footer />
+    </AppContainer>
+  );
+}
+
+export default App;
