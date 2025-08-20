d4be49c6e713b84806ad4e8e1b7ceb80a2b8e042
terrep263 <94027341+terrep263@users.noreply.github.com>
2025-08-20 13:42:26 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-@"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,4 +5,3 @@ export default defineConfig({
   plugins: [react()],
   envPrefix: ['VITE_', 'REACT_APP_']
 })
-"@ | Out-File -FilePath vite.config.js -Encoding UTF8
